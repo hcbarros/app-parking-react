7ab67bd3c80c8c1f0334d32dc77a2f1fa97115b6
hcbarros <48363350+hcbarros@users.noreply.github.com>
2021-04-16 19:44:31 -0300

    commit

--- src/components/validator.js
+++ src/components/validator.js
@@ -11,7 +11,9 @@ export default function validator(evt, input) {
              
     if(field.length === 8) return true;
 
-    if(field.charAt(4) === "-") field = field.replace("-","");
+    if(field.charAt(3) === "-" && field.charAt(4) === "-") {
+      field = field.substring(0, 4) + field.substring(5);
+    }
 
     if(!regexAll.test(char)) input.current.value = field.replace(char,"");
 
@@ -21,11 +23,11 @@ export default function validator(evt, input) {
       if(regexNum.test(char)) field = field.replace( /(.{3})(.)/,"$1-$2" );
       else field = field.substring(0, field.length - 1);  
     }
-    else if(field.length === 6 && !regexAll.test(char)) field = field.substring(0, field.length - 1); 
 
-    else if(field.length > 6 && !regexNum.test(char)) field = field.substring(0, field.length - 1);        
+    else if(field.length > 4 && !regexNum.test(char)) field = field.substring(0, field.length - 1);        
 
     input.current.value = field;                        
 
     return false
-  }
+}
+  
